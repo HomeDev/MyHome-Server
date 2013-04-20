17790bfe2963c1c030bd3875c8b809682f70f090
Andrey Krivko <jastkand@gmail.com>
2013-04-21 01:28:13 +0700

    add notification of all registered devices

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,28 +9,22 @@ class HomeController < ApplicationController
     if @notification.save
 
       logger.debug params.as_json
-      logger.debug @notification.body
 
-      #Urbanairship.register_device('DEVICE-TOKEN')
+      devices = Device.all.map{|a| a.token}
 
       notification = {
           :schedule_for => [1.seconds.from_now],
-          :apids => ['9d3553c4-7369-40d0-9d4e-3c42cb18520d', '7b756d8f-001d-4009-8701-3f645991d528'],
+          :apids => devices,
           :android => {
               :alert => @notification.body, :extra => {:foo => 'bar'}
           }
       }
 
-        #notify_all(@notification.body)
       response = Urbanairship.push(notification)
-      logger.debug response.as_json
 
-    #Urbanairship.register_device('7b756d8f-001d-4009-8701-3f645991d528', :provider => :android)
-      #render json: params.as_json
+      logger.debug response.as_json    
     end
     redirect_to root_path
   end
-
-private
 
 end
